6160e71a93246a79cf938916e54dfd22d16d1b64
J2ghz <j2.00ghz@gmail.com>
2019-10-01 21:47:54 +0200

    create bookingmanager for each test instead of once

--- HotelBooking.UnitTests/Services/BookingManagerTests.cs
+++ HotelBooking.UnitTests/Services/BookingManagerTests.cs
@@ -11,21 +11,23 @@ namespace HotelBooking.UnitTests.Services
 {
     public class BookingManagerTests
     {
-        private IBookingManager bookingManager;
-
-        public BookingManagerTests(){
-            DateTime start = DateTime.Today.AddDays(10);
-            DateTime end = DateTime.Today.AddDays(20);
-            IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
-            IRepository<Room> roomRepository = new FakeRoomRepository();
-            bookingManager = new BookingManager(bookingRepository, roomRepository);
+        private static IBookingManager BookingManager
+        {
+            get
+            {
+                var start = DateTime.Today.AddDays(10);
+                var end = DateTime.Today.AddDays(20);
+                IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
+                IRepository<Room> roomRepository = new FakeRoomRepository();
+                return new BookingManager(bookingRepository, roomRepository);
+            }
         }
-
+      
         [Fact]
         public void FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException()
         {
             DateTime date = DateTime.Today;
-            Assert.Throws<ArgumentException>(() => bookingManager.FindAvailableRoom(date, date));
+            Assert.Throws<ArgumentException>(() => BookingManager.FindAvailableRoom(date, date));
         }
 
         [Fact]
@@ -34,7 +36,7 @@ namespace HotelBooking.UnitTests.Services
             // Arrange
             DateTime date = DateTime.Today.AddDays(1);
             // Act
-            int roomId = bookingManager.FindAvailableRoom(date, date);
+            int roomId = BookingManager.FindAvailableRoom(date, date);
             // Assert
             Assert.NotEqual(-1, roomId);
         }
@@ -42,20 +44,20 @@ namespace HotelBooking.UnitTests.Services
         [Fact]
         public void GetFullyOccupiedDates_InvalidDates()
         {
-            Assert.Throws<ArgumentException>(() => bookingManager.GetFullyOccupiedDates(DateTime.MaxValue, DateTime.MinValue));
+            Assert.Throws<ArgumentException>(() => BookingManager.GetFullyOccupiedDates(DateTime.MaxValue, DateTime.MinValue));
         }
 
         [Fact(Skip = "I don't know how to fix this")]
         public void GetFullyOccupiedDates_All()
         {
-            var dates = bookingManager.GetFullyOccupiedDates(DateTime.MinValue, DateTime.MaxValue);
+            var dates = BookingManager.GetFullyOccupiedDates(DateTime.MinValue, DateTime.MaxValue);
             dates.Should().HaveCount(10);
         }
 
         [Fact]
         public void FindAvailableRoom_StartDateOk_EndDateConflict()
         {
-            bookingManager.FindAvailableRoom(DateTime.Today.AddDays(5), DateTime.Today.AddDays(15)).Should().Be(-1);
+            BookingManager.FindAvailableRoom(DateTime.Today.AddDays(5), DateTime.Today.AddDays(15)).Should().Be(-1);
         }
 
         [Property]
@@ -63,11 +65,11 @@ namespace HotelBooking.UnitTests.Services
         {
             if (start > end)
             {
-                Assert.Throws<ArgumentException>(() => bookingManager.GetFullyOccupiedDates(start, end));
+                Assert.Throws<ArgumentException>(() => BookingManager.GetFullyOccupiedDates(start, end));
             }
             else
             {
-                bookingManager.GetFullyOccupiedDates(start, end).Should()
+                BookingManager.GetFullyOccupiedDates(start, end).Should()
                     .NotBeNull().And
                     .NotContainNulls().And
                     .HaveCountLessOrEqualTo(11);
