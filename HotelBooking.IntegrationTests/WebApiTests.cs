a90466c625890f6e5ebb04d72235106a4957370f
J2ghz <j2.00ghz@gmail.com>
2019-10-11 20:42:05 +0200

    add webapi.startup test

---
+++ HotelBooking.IntegrationTests/WebApiTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace HotelBooking.IntegrationTests
+{
+    public class WebApiTests : IClassFixture<WebApplicationFactory<WebApi.Startup>>
+    {
+        private readonly WebApplicationFactory<WebApi.Startup> _factory;
+
+        public WebApiTests(WebApplicationFactory<WebApi.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public void Startup()
+        {
+            _factory.Should().NotBeNull();
+            _factory.CreateClient().Should().NotBeNull();
+        }
+    }
+}
